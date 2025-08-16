2f9fb6f1a242ba51fb63d705b6b45879a0b263f7
Timur Gorashchenko <timyr98239718@mail.ru>
2025-08-16 19:04:11 +0300

    use black for processing

--- src/processing.py
+++ src/processing.py
@@ -10,11 +10,11 @@ def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
 
     return new_data
 
+
 def sort_by_date(data: list[dict], is_reverse: bool = True) -> list[dict]:
     """
     Сортирует список словарей по значению ключа 'date'
     """
-    sorted_by_date_data = sorted(data, key=lambda date: date['date'], reverse = is_reverse)
+    sorted_by_date_data = sorted(data, key=lambda date: date["date"], reverse=is_reverse)
 
     return sorted_by_date_data
-
