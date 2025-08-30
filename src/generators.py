939251433e7be9b1a68ee9b0706a2180b0eba78b
Timur Gorashchenko <timyr98239718@mail.ru>
2025-08-30 21:43:15 +0300

    create filter_by_curency in src/processing

--- src/generators.py
+++ src/generators.py
@@ -1,2 +1,12 @@
-def filter_by_currency():
-    pass
+from typing import Dict, List, Iterator
+
+
+def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[Dict]:
+    '''
+    Фукнция для возвращения итератора, которая поочередно
+    выдаёт транзакции, где валюта операции соответсвует заданной
+    '''
+    result = (transaction for transaction in transactions
+              if transaction.get('operationAmount', {}).get('currency', {}).get('code') == currency)
+    return result
+
