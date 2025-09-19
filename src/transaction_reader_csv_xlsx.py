f28971af90a46734687ec27e1f79457b4a907d44
Timur Gorashchenko <timyr98239718@mail.ru>
2025-09-20 02:18:41 +0300

    fix

--- src/transaction_reader_csv_xlsx.py
+++ src/transaction_reader_csv_xlsx.py
@@ -1,17 +1,20 @@
 from typing import List, Dict
 import pandas as pd
+import os
 
 
-def read_transaction_csv(file_path:str) -> List[Dict]:
+
+def read_transactions_csv(file_path:str) -> List[Dict]:
     """
     Функция для считывания финансовых операций из CSV
     """
     csv_data = pd.read_csv(file_path)
     return csv_data.to_dict(orient='records')
 
-def read_transaction_xlsx(file_path:str) -> List[Dict]:
+def read_transactions_xlsx(file_path:str) -> List[Dict]:
     """
     Функция для считывания финансовых операций из Excel
     """
-    xlsx_data = pd.read_xml(file_path)
+    xlsx_data = pd.read_excel(file_path)
     return xlsx_data.to_dict(orient='records')
+
