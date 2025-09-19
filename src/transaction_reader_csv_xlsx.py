bdbd636692bb7d9883c5283187a74c0ad9e45c88
Timur Gorashchenko <timyr98239718@mail.ru>
2025-09-20 02:09:36 +0300

    add new function for read csv/xlsx files

---
+++ src/transaction_reader_csv_xlsx.py
@@ -0,0 +1,17 @@
+from typing import List, Dict
+import pandas as pd
+
+
+def read_transaction_csv(file_path:str) -> List[Dict]:
+    """
+    Функция для считывания финансовых операций из CSV
+    """
+    csv_data = pd.read_csv(file_path)
+    return csv_data.to_dict(orient='records')
+
+def read_transaction_xlsx(file_path:str) -> List[Dict]:
+    """
+    Функция для считывания финансовых операций из Excel
+    """
+    xlsx_data = pd.read_xml(file_path)
+    return xlsx_data.to_dict(orient='records')
